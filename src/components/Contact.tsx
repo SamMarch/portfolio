53f17f7fca29ba6fdfa35253c83b795e6518e09c
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 22:57:36 +1030

    Add contact form to Contact component

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import styles from '../styles/Contact.module.css';
 
 const Contact: React.FC = () => {
     return (
-        <div>
-            <h2>Contact Me</h2>
-            <p>Email: <a href="mailto:marchsam1988@gmail.com"> marchsam1988@gmail.com</a></p>
-            <p>GitHub: <a href="https://github.com/SamMarch"> Sam March</a></p>
-            <p>LinkedIn: <a href="https://www.linkedin.com/in/sam-march-1a2b3c4d/"> Sam March</a></p>
+        <div className={styles.contactContainer}>
+            <h2 className={styles.contactHeader}>Contact Me</h2>
+            <form className={styles.contactForm}>
+                <label className={styles.contactLabel} htmlFor="name">Name</label>
+                <input className={styles.contactInput} type="text" id="name" name="name" />
+
+                <label className={styles.contactLabel} htmlFor="email">Email</label>
+                <input className={styles.contactInput} type="email" id="email" name="email" />
+
+                <label className={styles.contactLabel} htmlFor="message">Message</label>
+                <textarea className={styles.contactInput} id="message" name="message"></textarea>
+
+                <button className={styles.contactButton} type="submit">Send</button>
+            </form>
         </div>
     );
 }

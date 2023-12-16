779fe7961a3b2de9d23cac66770e0adb8b1d9c31
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 22:05:33 +1030

    Add new components to App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
 import Portfolio from './components/Portfolio';
+import Projects from './components/Projects';
+import About from './components/About';
+import Contact from './components/Contact';
+import Resume from './components/Resume';
+
 
 function App() {
   return (
     <div className="App">
       <Portfolio />
+      <About />
+      <Projects />
+      <Resume />
+      <Contact />
     </div>
   );
 }

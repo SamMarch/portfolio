862bd0466514c9160f85899c206a6f82077186bd
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 22:05:03 +1030

    Add Contact component

---
+++ src/components/Contact.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Contact: React.FC = () => {
+    return (
+        <div>
+            <h2>Contact Me</h2>
+            <p>Email: <a href="mailto:marchsam1988@gmail.com"> marchsam1988@gmail.com</a></p>
+            <p>GitHub: <a href="https://github.com/SamMarch"> Sam March</a></p>
+            <p>LinkedIn: <a href="https://www.linkedin.com/in/sam-march-1a2b3c4d/"> Sam March</a></p>
+        </div>
+    );
+}
+
+export default Contact;

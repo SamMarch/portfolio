ae39b96cddce88ea57aa5a8cd3135f69838ec2c8
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 22:29:29 +1030

    Update navigation and styling in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,34 @@
 import React from 'react';
-import './App.css';
+import './styles/App.css';
+import styles from './styles/App.module.css';
 import Portfolio from './components/Portfolio';
 import Projects from './components/Projects';
 import About from './components/About';
 import Contact from './components/Contact';
 import Resume from './components/Resume';
-
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <Portfolio />
-      <About />
-      <Projects />
-      <Resume />
-      <Contact />
-    </div>
+    <Router>
+      <div className="App">
+        <nav className={styles.nav}>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+        </nav>
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/resume" element={<Resume />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/" element={<Portfolio />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 

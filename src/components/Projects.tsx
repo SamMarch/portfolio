13582988e98a644576326cbb0844289975ee8d51
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 22:57:17 +1030

    Add styles to Projects component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import styles from '../styles/Projects.module.css';
 
 const Projects: React.FC = () => {
     return (
-        <div>
+        <div className={styles.projectsContainer}>
             <h2>My Projects</h2>
-            <p>Coming soon!</p>
+            <div className={styles.projectCard}>
+                <h3 className={styles.projectTitle}>Project 1</h3>
+                <p className={styles.projectDescription}>This is a description of my first project. It's a web application built with React and TypeScript.</p>
+            </div>
+            <p>More coming soon!</p>
         </div>
     );
 }

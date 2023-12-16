7e4ba6082fcd5c5d94b3c276ff7abfad77789627
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 22:41:22 +1030

    Refactor About component with CSS modules

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import styles from '../styles/About.module.css';
 
 const About: React.FC = () => {
     return (
-        <div>
-            <h2>About Me</h2>
-            <p>I am a full-stack web developer with a passion for learning and problem solving. I am a graduate of the University of Adelaide Coding Boot Camp, where I learned the MERN stack. I am currently working on many projects to showcase my skills. I am excited to continue to learn and grow as a developer.</p>
+        <div className={styles.aboutContainer}>
+            <h2 className={styles.aboutHeader}>About Me</h2>
+            <p className={styles.aboutContent}>I am a full-stack web developer with a passion for learning and problem solving. I am a graduate of the University of Adelaide Coding Boot Camp, where I learned the MERN stack. I am currently working on many projects to showcase my skills. I am excited to continue to learn and grow as a developer.</p>
         </div>
     );
 }

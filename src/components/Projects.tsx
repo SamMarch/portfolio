9dab81a1df1850e7ad425667069dc85681cc946c
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 22:05:10 +1030

    Add Projects component

---
+++ src/components/Projects.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Projects: React.FC = () => {
+    return (
+        <div>
+            <h2>My Projects</h2>
+            <p>Coming soon!</p>
+        </div>
+    );
+}
+
+export default Projects;

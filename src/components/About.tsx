859816b15074e68e0914b4c76ce65cd02f907f8a
Sam March <106060649+SamMarch@users.noreply.github.com>
2023-12-16 22:04:52 +1030

    Add About component

---
+++ src/components/About.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const About: React.FC = () => {
+    return (
+        <div>
+            <h2>About Me</h2>
+            <p>I am a full-stack web developer with a passion for learning and problem solving. I am a graduate of the University of Adelaide Coding Boot Camp, where I learned the MERN stack. I am currently working on many projects to showcase my skills. I am excited to continue to learn and grow as a developer.</p>
+        </div>
+    );
+}
+
+export default About;
